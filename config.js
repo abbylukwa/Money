8ad7a08c6c52024b81da24489f89e7ec223b74b8
abbylukwa <ebenezerlukwa@gmail.com>
2025-10-27 13:44:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     PORT: process.env.PORT || 3000,
-    BASE_URL: process.env.BASE_URL || "http://localhost:3000"
+    BASE_URL: process.env.BASE_URL || "http://localhost:3000",
+    SESSION_ID: process.env.SESSION_ID || "ABBY_WHATSAPP_BOT",
+    ADMINS: ['263717457592@s.whatsapp.net', '263777627210@s.whatsapp.net', '27614159817@s.whatsapp.net']
 };
