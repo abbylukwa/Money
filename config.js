e58b7333cec075b9f39641c2b6fc3e4083c36750
abbylukwa <ebenezerlukwa@gmail.com>
2025-11-06 05:39:55 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,99 @@
-require('dotenv').config();
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
 
-module.exports = {
-    PORT: process.env.PORT || 3000,
-    BASE_URL: process.env.BASE_URL || "http://localhost:3000",
-    ADMINS: ['263717457592@s.whatsapp.net', '263777627210@s.whatsapp.net', '27614159817@s.whatsapp.net'],
-    // QR Code settings
-    QR_TIMEOUT: 60000, // 1 minute
-    MAX_RECONNECTION_ATTEMPTS: 5
-};
+import dotenv from 'dotenv'
+dotenv.config()
+
+const defaultOwner = '2347045889973';
+
+
+// Check for the OWNERS environment variable; if not found, use the default
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '' ; // put your number here
+
+const ownerlist = ownervb.split(';');
+
+global.owner = [];
+for (let i = 0; i < ownerlist.length; i++) {
+    global.owner.push([ownerlist[i], true]);
+}
+//
+global.botname = process.env.BOTNAME || 'AUTOBOB';
+global.mods = []
+global.prems = []
+global.allowed = ['2347045889973', '923051391007']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = [
+  '29d4b59a4aa687ca',
+  '5LTV57azwaid7dXfz5fzJu',
+  'cb15ed422c71a2fb',
+  '5bd33b276d41d6b4',
+  'HIRO',
+  'kurrxd09',
+  'ebb6251cc00f9c63',
+]
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['GataDios']
+
+global.canal = 'https://whatsapp.com'
+
+
+global.APIs = {
+  // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz',
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api.fgmods.xyz',
+}
+global.APIKeys = {
+  // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': `${keysneoxr}`,
+  'https://violetics.pw': 'beta',
+  'https://zenzapis.xyz': `${keysxxx}`,
+  'https://api.fgmods.xyz': 'm2XBbNvz',
+}
+
+// Sticker WM
+global.premium = 'true'
+global.packname = 'AUTOBOB'
+global.author = 'BobbyX208'
+global.menuvid = 'https://i.imgur.com/2U2K9YA.mp4'
+global.igfg = ' Follow on Instagram\nhttps://www.instagram.com/bobbyvic23'
+global.dygp = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.fgsc = 'https://github.com/BobbyX208/AUTOBOB'
+global.fgyt = 'https://youtube.com/@BobbyX208'
+global.fgpyp = 'https://youtube.com/@BobbyX208'
+global.fglog = 'https://i.ibb.co/G2dh9cB/qasim.jpg'
+global.thumb = fs.readFileSync('./assets/A.jpg')
+
+global.wait = '⏳'
+global.rwait = '⏳'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❌'
+global.xmoji = '🤩'
+
+global.multiplier = 69
+global.maxwarn = '3'
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
