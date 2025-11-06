d367fd7520a08259de08dc43e0ceb5bbfadd28da
abbylukwa <ebenezerlukwa@gmail.com>
2025-11-06 07:50:38 +0200

    Refactor print.js to use CommonJS and add logging

--- print.js
+++ print.js
@@ -1,50 +1,23 @@
-import chalk from 'chalk'
-import { watchFile } from 'fs'
+const chalk = require("chalk");
 
-const terminalImage = global.opts['img'] ? require('terminal-image') : ''
-const urlRegex = (await import('url-regex-safe')).default({ strict: false })
+const terminalLogs = [];
 
-const log = (text, error = false) =>
-  console.log(
-    chalk[error ? 'red' : 'blue']('[MEGA AI]'),
-    chalk[error ? 'redBright' : 'greenBright'](text)
-  )
-
-export default async function (m, conn = { user: {} }) {
-  let senderName = await conn.getName(m.sender)
-
-  let chatName = ''
-  if (m.chat && m.chat !== m.sender) {
-    if (!m.chat.endsWith('@g.us')) {
-      chatName = 'Private'
-    } else {
-      chatName = await conn.getName(m.chat)
-      chatName = chatName ? `${chatName} ` : ''
+function logToTerminal(message) {
+    const logEntry = {
+        id: terminalLogs.length + 1,
+        message: message,
+        timestamp: new Date()
+    };
+    terminalLogs.push(logEntry);
+    console.log(chalk.blue(`[${new Date().toLocaleTimeString()}]`) + ' ' + message);
+    
+    if (terminalLogs.length > 50) {
+        terminalLogs.shift();
     }
-  } else {
-    chatName = 'Private'
-  }
-
-  if (m.isCommand) {
-    let commandText = m.text.split(' ')[0]
-    const cmdtxt = chalk.cyanBright('Command')
-    const cmd = chalk.yellowBright(`${commandText}`)
-    const from = chalk.greenBright('from')
-    const username = chalk.yellowBright(`${senderName}`)
-    const ins = chalk.greenBright('in')
-    const grp = chalk.blueBright(chatName)
-    log(`${cmdtxt} ${cmd} ${from} ${username} ${ins} ${grp}`)
-  } else {
-    const msg = chalk.cyanBright('Message')
-    const from = chalk.greenBright('from')
-    const username = chalk.yellowBright(`${senderName}`)
-    const ins = chalk.greenBright('in')
-    const grp = chalk.blueBright(chatName)
-    log(`${msg} ${from} ${username} ${ins} ${grp}`)
-  }
 }
 
-let file = global.__filename(import.meta.url)
-watchFile(file, () => {
-  log(chalk.redBright("Update 'lib/print.js'"), false)
-})
+function getTerminalLogs() {
+    return terminalLogs;
+}
+
+module.exports = { logToTerminal, getTerminalLogs };
